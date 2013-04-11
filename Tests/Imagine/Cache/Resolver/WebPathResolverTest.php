klj613/LiipImagineBundle

commit 07228a5d7bf1382a1843c3807e371853bf98c754

Use the basePath in the file path resolver (useful in "_dev" or "_*" environments when the script name is present in the url)

Imagine/Cache/Resolver/WebPathResolver.php
@@ -31,7 +31,8 @@ class WebPathResolver extends AbstractFilesystemResolver implements CacheManager
     public function resolve(Request $request, $path, $filter)
     {
         $browserPath = $this->decodeBrowserPath($this->getBrowserPath($path, $filter));
-        $targetPath = $this->getFilePath($path, $filter, $request->getBaseUrl());
+        $this->basePath = $request->getBaseUrl();
+        $targetPath = $this->getFilePath($path, $filter);
 
         // if the file has already been cached, we're probably not rewriting
         // correctly, hence make a 301 to proper location, so browser remembers

commit a3a8456278206ff86ae296542817032720346957

Fixes base path issue
Fixes issue when you try to save to /foo/bar and the URL is
/media/cache/baz.jpg it will actually save into
/foo/bar/media/cache/baz.jpg when the expected result was
/foo/bar/cache/baz/jpg since the cache_base_path configuration was set.

This _may_ of been caused by commit
07228a5d7bf1382a1843c3807e371853bf98c754 which fixed the app_*.php
base URL issues. The baseUrl was set to the basePath variable which was
overriding the cache_base_path configuration in all situations which
made the configuration pointless.

The solution was to set the request's baseUrl into a new variable
baseUrl instead of the basePath therefore the basePath will be able to
be configured.

Imagine/Cache/Resolver/AbstractFilesystemResolver.php
@@ -23,6 +23,11 @@ abstract class AbstractFilesystemResolver implements ResolverInterface, CacheMan
     protected $basePath = '';
 
     /**
+     * @var string
+     */
+    protected $baseUrl = '';
+
+    /**
      * @var CacheManager
      */
     protected $cacheManager;

Imagine/Cache/Resolver/WebPathResolver.php
@@ -14,7 +14,7 @@ class WebPathResolver extends AbstractFilesystemResolver
     public function resolve(Request $request, $path, $filter)
     {
         $browserPath = $this->decodeBrowserPath($this->getBrowserPath($path, $filter));
-        $this->basePath = $request->getBaseUrl();
+        $this->baseUrl = $request->getBaseUrl();
         $targetPath = $this->getFilePath($path, $filter);
 
         // if the file has already been cached, we're probably not rewriting
@@ -64,9 +64,10 @@ class WebPathResolver extends AbstractFilesystemResolver
     protected function getFilePath($path, $filter)
     {
         $browserPath = $this->decodeBrowserPath($this->getBrowserPath($path, $filter));
+        $base = $this->baseUrl . $this->basePath;
 
-        if (!empty($this->basePath) && 0 === strpos($browserPath, $this->basePath)) {
-            $browserPath = substr($browserPath, strlen($this->basePath));
+        if (!empty($base) && 0 === strpos($browserPath, $base)) {
+            $browserPath = substr($browserPath, strlen($base));
         }
 
         return $this->cacheManager->getWebRoot().$browserPath;

Tests/Imagine/Cache/Resolver/WebPathResolverTest.php
@@ -20,10 +20,12 @@ class WebPathResolverTest extends AbstractTest
      * @var WebPathResolver
      */
     protected $resolver;
+    protected $basePathResolver;
 
     protected $webRoot;
     protected $dataRoot;
     protected $cacheDir;
+    protected $basePathCacheDir;
 
     protected function setUp()
     {
@@ -44,8 +46,10 @@ class WebPathResolverTest extends AbstractTest
         $this->webRoot = $this->tempDir.'/root/web';
         $this->dataRoot = $this->fixturesDir.'/assets';
         $this->cacheDir = $this->webRoot.'/media/cache';
+        $this->basePathCacheDir = $this->webRoot.'/cache';
 
         $this->filesystem->mkdir($this->cacheDir);
+        $this->filesystem->mkdir($this->basePathCacheDir);
 
         $this->cacheManager = $this->getMock('Liip\ImagineBundle\Imagine\Cache\CacheManager', array(
             'generateUrl',
@@ -54,7 +58,11 @@ class WebPathResolverTest extends AbstractTest
         ));
 
         $this->resolver = new WebPathResolver($this->filesystem);
+        $this->basePathResolver = new WebPathResolver($this->filesystem);
+        $this->basePathResolver->setBasePath('/media');
+
         $this->cacheManager->addResolver('web_path', $this->resolver);
+        $this->cacheManager->addResolver('web_path', $this->basePathResolver);
     }
 
     public function testDefaultBehavior()
@@ -165,7 +173,7 @@ class WebPathResolverTest extends AbstractTest
     /**
      * @depends testDefaultBehavior
      */
-    public function testResolveWithBasePath()
+    public function testResolveWithBaseUrl()
     {
         $this->cacheManager
             ->expects($this->atLeastOnce())
@@ -201,6 +209,50 @@ class WebPathResolverTest extends AbstractTest
 
         // Remove the cached image.
         $this->assertTrue($this->resolver->remove($targetPath, 'thumbnail'),
+            '->remove() reports removal of cached image file correctly.');
+        $this->assertFalse(file_exists($targetPath),
+            '->remove() actually removes the cached file from the filesystem.');
+    }
+
+    /**
+     * @depends testDefaultBehavior
+     */
+    public function testResolveWithBasePath()
+    {
+        $this->cacheManager
+            ->expects($this->atLeastOnce())
+            ->method('generateUrl')
+            ->will($this->returnValue('/sandbox/app_dev.php/media/cache/thumbnail/cats.jpeg'))
+        ;
+
+        $request = $this->getMock('Symfony\Component\HttpFoundation\Request');
+        $request
+            ->expects($this->atLeastOnce())
+            ->method('getBaseUrl')
+            ->will($this->returnValue('/sandbox/app_dev.php'))
+        ;
+
+        // Resolve the requested image for the given filter.
+        $targetPath = $this->basePathResolver->resolve($request, 'cats.jpeg', 'thumbnail');
+        // The realpath() is important for filesystems that are virtual in some way (encrypted, different mount options, ..)
+        $this->assertEquals(realpath($this->basePathCacheDir).'/thumbnail/cats.jpeg', $targetPath,
+            '->resolve() correctly converts the requested file into target path within webRoot.');
+        $this->assertFalse(file_exists($targetPath),
+            '->resolve() does not create the file within the target path.');
+
+        // Store the cached version of that image.
+        $content = file_get_contents($this->dataRoot.'/cats.jpeg');
+        $response = new Response($content);
+        $this->basePathResolver->store($response, $targetPath, 'thumbnail');
+        $this->assertEquals(201, $response->getStatusCode(),
+            '->store() alters the HTTP response code to "201 - Created".');
+        $this->assertTrue(file_exists($targetPath),
+            '->store() creates the cached image file to be served.');
+        $this->assertEquals($content, file_get_contents($targetPath),
+            '->store() writes the content of the original Response into the cache file.');
+
+        // Remove the cached image.
+        $this->assertTrue($this->basePathResolver->remove($targetPath, 'thumbnail'),
             '->remove() reports removal of cached image file correctly.');
         $this->assertFalse(file_exists($targetPath),
             '->remove() actually removes the cached file from the filesystem.');